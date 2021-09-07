40b52a5a8664fa01f3f73c55cd1c1bc57ad116e9
a.sumin <a.sumin@cardsmobile.ru>
2021-09-07 19:26:02 +0300

    Initial commit

---
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.sumin.servicestest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.sumin.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
