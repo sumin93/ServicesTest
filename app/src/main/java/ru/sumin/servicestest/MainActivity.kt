e2799081963fabb7035f9df22e4384d0b0fa580b
a.sumin <a.sumin@cardsmobile.ru>
2021-09-07 20:00:01 +0300

    launch first service

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -13,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
---
+++ app/src/main/java/ru/sumin/servicestest/MyService.kt
@@ -0,0 +1,54 @@
+package ru.sumin.servicestest
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyService: Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyService: $message")
+    }
+
+    companion object {
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyService::class.java)
+        }
+    }
+}
