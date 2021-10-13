0d6d4a17b4073ae5e0c7e857222445a8738bbd9d
a.sumin <a.sumin@cardsmobile.ru>
2021-10-13 22:59:54 +0300

    Add WorkManager

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -8,6 +8,8 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import ru.sumin.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -56,6 +58,14 @@ class MainActivity : AppCompatActivity() {
         }
         binding.jobIntentService.setOnClickListener {
             MyJobIntentService.enqueue(this, page++)
+        }
+        binding.workManager.setOnClickListener {
+            val workManager = WorkManager.getInstance(applicationContext)
+            workManager.enqueueUniqueWork(
+                MyWorker.WORK_NAME,
+                ExistingWorkPolicy.APPEND,
+                MyWorker.makeRequest(page++)
+            )
         }
     }
 }
---
+++ app/src/main/java/ru/sumin/servicestest/MyWorker.kt
@@ -0,0 +1,48 @@
+package ru.sumin.servicestest
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+
+class MyWorker(
+    context: Context,
+    private val workerParameters: WorkerParameters
+) : Worker(context, workerParameters) {
+
+    override fun doWork(): Result {
+        log("doWork")
+        val page = workerParameters.inputData.getInt(PAGE, 0)
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+        return Result.success()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyWorker: $message")
+    }
+
+    companion object {
+
+        private const val PAGE = "page"
+
+        const val WORK_NAME = "work name"
+
+        fun makeRequest(page: Int): OneTimeWorkRequest {
+            return OneTimeWorkRequestBuilder<MyWorker>().apply {
+                setInputData(workDataOf(PAGE to page))
+                setConstraints(makeConstraints())
+            }.build()
+        }
+
+        private fun makeConstraints() = Constraints.Builder()
+            .setRequiresCharging(true)
+            .build()
+    }
+}
