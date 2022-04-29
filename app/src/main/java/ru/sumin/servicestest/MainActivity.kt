32d3fc995154d84dabdb5b7e58a68d1be5ae77ec
a.sumin <sumin93@mail.ru>
2022-04-29 19:07:16 +0300

    Bind service

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -4,8 +4,10 @@ import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.app.job.JobWorkItem
 import android.content.ComponentName
+import android.content.ServiceConnection
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.work.ExistingWorkPolicy
@@ -19,6 +21,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var page = 0
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = (service as? MyForegroundService.LocalBinder) ?: return
+            val foregroundService = binder.getService()
+            foregroundService.onProgressChanged = { progress ->
+                binding.progressBarLoading.progress = progress
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,5 +82,19 @@ class MainActivity : AppCompatActivity() {
                 MyWorker.makeRequest(page++)
             )
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(serviceConnection)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        bindService(
+            MyForegroundService.newIntent(this),
+            serviceConnection,
+            0
+        )
     }
 }
--- app/src/main/java/ru/sumin/servicestest/MyForegroundService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyForegroundService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
@@ -23,6 +24,8 @@ class MyForegroundService : Service() {
         createNotificationBuilder()
     }
 
+    var onProgressChanged: ((Int) -> Unit)? = null
+
     override fun onCreate() {
         super.onCreate()
         log("onCreate")
@@ -39,6 +42,7 @@ class MyForegroundService : Service() {
                     .setProgress(100, i, false)
                     .build()
                 notificationManager.notify(NOTIFICATION_ID, notification)
+                onProgressChanged?.invoke(i)
                 log("Timer $i")
             }
             stopSelf()
@@ -52,8 +56,9 @@ class MyForegroundService : Service() {
         log("onDestroy")
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+    override fun onBind(intent: Intent?): IBinder {
+        log("onBind")
+        return LocalBinder()
     }
 
     private fun log(message: String) {
@@ -77,6 +82,11 @@ class MyForegroundService : Service() {
         .setSmallIcon(R.drawable.ic_launcher_background)
         .setProgress(100, 0, false)
         .setOnlyAlertOnce(true)
+
+    inner class LocalBinder : Binder() {
+
+        fun getService() = this@MyForegroundService
+    }
 
     companion object {
 
