00de741ca9e92cd734c4cb9f6836810e44f1fc74
a.sumin <sumin93@mail.ru>
2022-05-04 14:52:20 +0300

    Add alarm manager

---
+++ app/src/main/java/ru/sumin/servicestest/AlarmReceiver.kt
@@ -0,0 +1,54 @@
+package ru.sumin.servicestest
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        context?.let {
+            val notificationManager = getSystemService(
+                it,
+                NotificationManager::class.java
+            ) as NotificationManager
+
+            createNotificationChannel(notificationManager)
+
+            val notification = NotificationCompat.Builder(it, CHANNEL_ID)
+                .setContentTitle("Title")
+                .setContentText("Text")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .build()
+
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+    }
+
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    companion object {
+
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+        private const val NOTIFICATION_ID = 1
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, AlarmReceiver::class.java)
+        }
+    }
+}
--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.sumin.servicestest
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.app.job.JobWorkItem
@@ -13,6 +15,7 @@ import androidx.core.content.ContextCompat
 import androidx.work.ExistingWorkPolicy
 import androidx.work.WorkManager
 import ru.sumin.servicestest.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -81,6 +84,14 @@ class MainActivity : AppCompatActivity() {
                 ExistingWorkPolicy.APPEND,
                 MyWorker.makeRequest(page++)
             )
+        }
+        binding.alarmManager.setOnClickListener {
+            val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.SECOND, 30)
+            val intent = AlarmReceiver.newIntent(this)
+            val pendingIntent = PendingIntent.getBroadcast(this, 100, intent, 0)
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
         }
     }
 
